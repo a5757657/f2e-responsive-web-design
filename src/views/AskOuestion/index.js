ac9aeb02032e3d1053e25c2108385a65d8c70690
Winnie Chien <winnie145ch@gmail.com>
2022-10-31 01:32:15 +0800

    feature: global.scss btn style /ButtonTest example

--- src/views/AskOuestion/index.js
+++ src/views/AskOuestion/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './index.scss'
+// import '../../styles/global.scss';
 
 const index = () => {
   return (
-    <div className='AskOuestion'><p>提出問題</p></div>
+    <div className='AskOuestion'>
+    <p>提出問題</p>
+    </div>
   )
 }
 
