8ae823e6aa0fe216eb5e8909e97d552728d60b95
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-10-31 23:22:25 +0800

    靜態頁面完成

--- src/views/Awards/index.js
+++ src/views/Awards/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import './index.scss'
+import { reviewMechanism, awards } from '../../asset/Awards/TextData'
 
 const Awards = () => {
   return (
-    <div className='Awards'><p>獎項設計</p></div>
+    <div className='Awards'>
+      <p>區區修煉已經無法滿足了嗎？還有比賽等著你！</p>
+      <h2>評審機制</h2>
+      <div className="selectionWrap">
+        {reviewMechanism.map((d, idx) => (
+          <div key={`${d.title}-${idx}`} className="awards">
+            <h3>{d.title}</h3>
+            <p>{d.info}</p>
+          </div>
+        ))}
+      </div>
+      <div className="titleWrap">
+        <h2>獎項</h2>
+        <span className="remark">皆含數位獎狀</span>
+      </div>
+      <div className="awardsWrap">
+        {awards.map((d, idx) => (
+          <div key={`${d.title}-${idx}`} className="awards">
+            <h3>{d.title}</h3>
+            <p>{d.info}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
