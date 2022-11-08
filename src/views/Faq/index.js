b7d8126fce78b2d54d1437d058247616e0c672ff
Wei-Chi <winnie145ch@gmail.com>
2022-11-08 14:45:16 +0800

    非主要靜態網站

--- src/views/Faq/index.js
+++ src/views/Faq/index.js
@@ -4,16 +4,27 @@ import './index.scss'
 const Faq = () => {
   return (
     <div className='Faq'>
-      <h5>常見問答</h5>
-      <div className='btns'>
-        <button className='btn btn-tag'>常見問答</button>
-        <button className='btn btn-tag'>UI設計師</button>
-        <button className='btn btn-tag'>前端工程師</button>
-        <button className='btn btn-tag'>團體組</button>
-      </div>
-      <div className='cotent'></div>
-      <div className='joinBtn'>
-        <button className='btn btn-cta'>別擔心太多。報名，就對了</button>
+      <div className="title">常見問答</div>
+      <div className="container">
+        <div className="bg"><img src=""/></div>
+        <div className="bg"><img src=""/></div>
+        <div className="wrapper">
+          <div className='btns'>
+            <button className='btn btn-tag'>常見問答</button>
+            <button className='btn btn-tag'>UI設計師</button>
+            <button className='btn btn-tag'>前端工程師</button>
+            <button className='btn btn-tag'>團體組</button>
+          </div>
+          <div className='cotent'>
+            <div className="FAQ"></div>
+            <div className="UI"></div>
+            <div className="FE"></div>
+            <div className="GP"></div>
+          </div>
+          <div className='joinBtn'>
+            <button className='btn btn-cta'>別擔心太多。報名，就對了</button>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/views/Sponsor/index.js
+++ src/views/Sponsor/index.js
@@ -1,25 +1,45 @@
 import React from "react";
 import "./index.scss";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 // Images
 import block from "./../../asset/AskQuesSponsor/Sponsor/BlockStudio.png"
 import titan from "./../../asset/AskQuesSponsor/Sponsor/TitanSoft.png"
 import kdan from "./../../asset/AskQuesSponsor/Sponsor/KDAN.png"
 
+// Links
+const titanLink = "https://titansoft.com/tw"
+const kdanLink = "https://www.kdanmobile.com/zh-tw"
+const blockLink = "https://blockstudio.tw"
+
 const Sponsor = () => {
   return (
     <div className="Sponsor">
+      <div className="bg">
+        <img src=""/>
+      </div>
       <div className="title">贊助單位</div>
       <div className="container">
         <div className="wrapper">
-          <div className="bg">
+          <LINK to={titanLink}>
+          <div className="blocks"  data-aos="fade-left" data-aos-anchor-placement="top-bottom">
+            <div className="word">鈦坦科技</div>
             <img id="titansoft" src={titan} alt="新加坡商 鈦坦科技" />
           </div>
-          <div className="bg">
-            <img id="kdan" src={block} alt="" />
+          </LINK>
+          <LINK to={kdanLink}>
+          <div className="blocks"  data-aos-anchor-placement="top-bottom">
+            <div className="word">凱鈿科技</div>
+            <img id="kdan" src={block} alt="凱鈿科技" />
           </div>
-          <div className="bg">
+          </LINK>
+          <LINK to={blockLink}>
+          <div className="blocks" data-aos="fade-right" data-aos-anchor-placement="top-bottom">
+            <div className="word">板塊設計</div>
             <img id="blockstudio" src={kdan} alt="板塊設計" />
           </div>
+          </LINK>
         </div>
         <div className="cooper">
           <p>共同推廣 JIRA、miro、DottedSign</p>
