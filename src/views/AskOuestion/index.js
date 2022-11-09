8da127168240aeb3752313a2c8897a59d96ea85b
Wei-Chi <winnie145ch@gmail.com>
2022-11-09 14:03:20 +0800

    unsaved

--- src/views/AskOuestion/index.js
+++ src/views/AskOuestion/index.js
@@ -8,25 +8,32 @@ const index = () => {
       <div className="title">你是否也有以下困擾？</div>
       <div className="container">
         <div className="grid">
-          <img id="" src="" alt="" />
-          <div className="Q1">
-            <p>羨慕別人的酷酷網頁動畫？</p>
-          </div>
-          <div className="box">
-            <div className="Q2">
-              <p>滿足不了同事的許願？</p>
+          <div className="block">
+            <div className="picGroup">
+              <img id="" className="pic pic1" src="" alt=""/>
+              <img id="" className="pic pic2" src="" alt=""/>
+              <img id="" className="pic pic3" src="" alt=""/>
+              <img id="" className="pic pic4" src="" alt=""/>
             </div>
-            <img id="" className="none" src="" alt="" />
+            <div className="Ques">羨慕別人的酷酷網頁動畫？</div>
           </div>
-          <div className="box">
-            <img id="" className="" src="" alt="" />
-            <div className="Q2 none">
-              <p>滿足不了同事的許願？</p>
+          <div className="block">
+          <div className="picGroup">
+              <img id="" className="pic pic1" src="" alt=""/>
+              <img id="" className="pic pic2" src="" alt=""/>
+              <img id="" className="pic pic3" src="" alt=""/>
+              <img id="" className="pic pic4" src="" alt=""/>
             </div>
+            <div className="Ques">滿足不了同事的許願？</div>
           </div>
-          <img id="" src="" alt="" />
-          <div className="Q3">
-            <p>動畫技能樹太雜無從下手？</p>
+          <div className="block">
+          <div className="picGroup">
+              <img id="" className="pic pic1" src="" alt=""/>
+              <img id="" className="pic pic2" src="" alt=""/>
+              <img id="" className="pic pic3" src="" alt=""/>
+              <img id="" className="pic pic4" src="" alt=""/>
+            </div>
+            <div className="Ques">動畫技能樹太雜無從下手？</div>
           </div>
         </div>
       </div>
